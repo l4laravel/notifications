0e18ca67bc73c85c9c632440a46b43f582bf89a3
rohanhacker <rohanvfx15@gmail.com>
2018-03-24 16:50:11 +0530

    send me whow is not our database

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,12 @@ Route::get('/', function () {
 
     $when = now()->addSecond(10);
 
-    $users->notify((new TaskCompleted)->delay($when));
+    //$users->notify((new TaskCompleted)->delay($when));
 
+    // send mail who is not our database
 
+    Notification::route('mail', 'nishadhiman@laravel.com')
+        ->notify(new TaskCompleted);
 
     return view('welcome');
 });
